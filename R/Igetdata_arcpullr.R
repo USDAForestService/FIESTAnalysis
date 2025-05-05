e4a93d8ec8a5757eafa482ff0ff7560db6bc7ec4
Joshua-Yamamoto <joshua.yamamoto@usda.gov>
2025-05-05 08:40:20 -0700

    export getdata_arcpullr functions for vignette use

--- R/Igetdata_arcpullr.R
+++ R/Igetdata_arcpullr.R
@@ -1,8 +1,12 @@
+#' get NFS
+#'
+#' @param region NFS region
+#' @export
 getnfs <- function(region) {
-  
+
   URLadminfs <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_ForestSystemBoundaries_01/MapServer/0"
   ## US Forest Service Administrative Forest Boundaries
-  
+
   ## R1 - Northern region (MT, ND, IDn)
   ## R2 - Rocky Mountain Region (CO, WY, KA, NE, SD)
   ## R3 - Southwestern Region (AZ, NM)
@@ -12,36 +16,40 @@ getnfs <- function(region) {
   ## R8 - Southern Region (TX, OK, AR, LA, MI, AL, TE, GE, FL, NC, SC, VA, KY)
   ## R9 - Eastern Region (MN, IA, MO, WI, IL, MI, IN, OH, NY, PA, WV, MD, DE, NJ, CT, RI, MA, ME, VT, NH)
   ## R10 - Alaska Region (AK)
-  
+
   regionlst <- c(1:6,8:10)
   if (!region %in% regionlst) {
     stop("region must be in: ", toString(regionlst))
   }
-  
+
   regionC <- sapply(region, formatC, digits=2, width=2, flag="0")
   region.filter <- FIESTAutils::getfilter("REGION", regionC, syntax="sql")
-  
-  
+
+
   ## Get Administrative forest boundaries
   nfsbnd <- arcpullr::get_spatial_layer(url = URLadminfs,
                                        where = region.filter)
-  
+
   ## check validation of polygons
   if (!any(sf::st_is_valid(nfsbnd))) {
     nfsbnd <- FIESTAutils::polyfix.sf(nfsbnd)
   }
-  
+
   return(nfsbnd)
 }
 
 
-
+#' get District
+#'
+#' @param forest Forest Name
+#' @param region NFS region
+#' @export
 getdistrict <- function(forest = NULL, region = NULL) {
-  
+
   URLdistricts <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RangerDistricts_01/MapServer/0"
   ## US Forest Service Ranger District Boundaries
-  
-  
+
+
   FORESTNAMES <- c('Allegheny National Forest','Angeles National Forest','Apache-Sitgreaves National Forests',
                    'Arapaho and Roosevelt National Forests','Ashley National Forest',
                    'Beaverhead-Deerlodge National Forest','Bighorn National Forest','Bitterroot National Forest',
@@ -75,10 +83,10 @@ getdistrict <- function(forest = NULL, region = NULL) {
                    'Tahoe National Forest','Tongass National Forest','Tonto National Forest',
                    'Uinta-Wasatch-Cache National Forest','Umatilla National Forest','Umpqua National Forest',
                    'Wallowa-Whitman National Forest','Wayne National Forest','White Mountain National Forest','White River National Forest')
-  
-  
+
+
   regionlst <- c(1:6,8:10)
-  
+
   if (is.null(forest)) {
     if (!is.null(region)) {
       if (!region %in% regionlst) {
@@ -88,52 +96,55 @@ getdistrict <- function(forest = NULL, region = NULL) {
       ## select region from dropdown menu
       region <- select.list(regionlst, title="region", multiple=TRUE)
     }
-    
+
     regionC <- sapply(region, formatC, digits=2, width=2, flag="0")
     region.filter <- FIESTAutils::getfilter("REGION", regionC, syntax="sql")
-    
-    
+
+
     ## Get Administrative forest district boundaries
     ALPregion <- arcpullr::get_spatial_layer(url = URLalp,
                                              where = region.filter)
     FORESTNAMES <- sort(unique(ALPregion$FORESTNAME))
-    
+
     ## Select forest from dropdown menu
     forest <- select.list(FORESTNAMES, title="forest names", multiple=TRUE)
-    
+
   } else {
-    
+
     if (!all(forest %in% FORESTNAMES)) {
       message("invalid forest: \n", toString(FORESTNAMES))
       forest <- select.list(FORESTNAMES, title="forest names", multiple=TRUE)
     }
   }
-  
-  
+
+
   ## Get forest filter
   forest.filter <- paste0("FORESTNAME IN(", toString(encodeString(forest, quote="'")), ")")
-  
-  
+
+
   ## Get Administrative forest district boundaries
   districts <- arcpullr::get_spatial_layer(url = URLdistricts,
                                            where = forest.filter)
-  
+
   ## check validation of polygons
   if (!any(sf::st_is_valid(districts))) {
     districts <- FIESTAutils::polyfix.sf(districts)
   }
-  
+
   return(districts)
 }
 
 
-
+#' get NFS watershed
+#'
+#' @param forest Forest Name
+#' @export
 getNFSwatershed <- function(forest = NULL) {
-  
+
 
   URLwatershedsNFS <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_PriorityWatersheds_01/MapServer/0"
   ## U.S. Forest Service Priority Watersheds - HUC12 (from the Watershed Boundary Dataset)
-  
+
   forest_names <- c('Allegheny National Forest','Angeles National Forest','Apache-Sitgreaves National Forest',
                     'Arapaho & Roosevelt National Forests','Ashley National Forest','Beaverhead-Deerlodge National Forest',
                     'Bighorn National Forest','Bitterroot National Forest','Black Hills National Forest',
@@ -177,55 +188,59 @@ getNFSwatershed <- function(forest = NULL) {
                     'Uinta-Wasatch-Cache National Forest','Umatilla National Forest','Umpqua National Forest',
                     'Wallowa Whitman National Forest','Wayne National Forest','White Mountain National Forest',
                     'White River National Forest','Willamette National Forest')
-  
+
   if (!is.null(forest)) {
     if (!forest %in% forest_names) {
       message("forest is not in layer: ")
       print(as.data.frame(forest_names))
       stop()
     }
-  }	
+  }
   forest.filter <- FIESTAutils::getfilter("FOREST_NAME", forest, syntax="sql")
-  
+
   ## Define out_fields
   out_fields <- c("FOREST_NAME", "WATERSHED_CODE", "WATERSHED_NAME",
                   "WATERSHED_CONDITION_FS_AREA", "TOTAL_WATERSHED_AREA_ACRES",
                   "FS_OWNERSHIP_PERCENT")
   if (!is.null(forest)) {
     forest.filter <- FIESTAutils::getfilter("FOREST_NAME", forest, syntax="sql")
-    
+
     ## Get Watersheds
     watershedNFS <- arcpullr::get_spatial_layer(url = URLwatershedsNFS,
                                                 out_fields = out_fields,
                                                 where = forest.filter)
   } else {
-    
+
     ## Get WatershedsNFS
     watershedNFS <- arcpullr::get_spatial_layer(url = URLwatershedsNFS,
                                                 out_fields = out_fields)
   }
-  
+
   ## check validation of polygons
   if (!any(sf::st_is_valid(watershedNFS))) {
     watershedNFS <- FIESTAutils::polyfix.sf(watershedNFS)
   }
-  
-  
+
+
   return(watershedNFS)
 }
 
 
-
+#' get ALP
+#'
+#' @param forest Forest Name
+#' @param region NFS region
+#' @export
 getALP <- function(forest = NULL, region = NULL) {
-  
+
   options(timeout = max(1000, getOption("timeout")))
   #Sys.setenv(ALL_PROXY = "YOUR_PROXY_HOST")
-  
+
   URLalp <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_BasicOwnership_01/MapServer/0"
   #URLalp_info <- arcpullr::get_layer_info(url = URLalp)
   #URLalp_info$fields$name
-  
-  
+
+
   FORESTNAMES <- c('Allegheny National Forest','Angeles National Forest','Apache-Sitgreaves National Forests',
                    'Arapaho and Roosevelt National Forests','Ashley National Forest',
                    'Beaverhead-Deerlodge National Forest','Bighorn National Forest','Bitterroot National Forest',
@@ -259,10 +274,10 @@ getALP <- function(forest = NULL, region = NULL) {
                    'Tahoe National Forest','Tongass National Forest','Tonto National Forest',
                    'Uinta-Wasatch-Cache National Forest','Umatilla National Forest','Umpqua National Forest',
                    'Wallowa-Whitman National Forest','Wayne National Forest','White Mountain National Forest','White River National Forest')
-  
-  
+
+
   regionlst <- c(1:6,8:10)
-  
+
   if (is.null(forest)) {
     if (!is.null(region)) {
       if (!region %in% regionlst) {
@@ -272,86 +287,88 @@ getALP <- function(forest = NULL, region = NULL) {
       ## select region from dropdown menu
       region <- select.list(regionlst, title="region", multiple=TRUE)
     }
-  
+
     regionC <- sapply(region, formatC, digits=2, width=2, flag="0")
     region.filter <- FIESTAutils::getfilter("REGION", regionC, syntax="sql")
-  
-  
+
+
     ## Get Administrative forest district boundaries
     ALPregion <- arcpullr::get_spatial_layer(url = URLalp,
                                            where = region.filter)
-    
+
     ## check validation of polygons
     if (!any(sf::st_is_valid(ALPregion))) {
       ALPregion <- FIESTAutils::polyfix.sf(ALPregion)
     }
-    
+
     return(ALPregion)
-    
+
 #    FORESTNAMES <- sort(unique(ALPregion$FORESTNAME))
-    
+
 #    ## Select forest from dropdown menu
 #    forest <- select.list(FORESTNAMES, title="forest names", multiple=TRUE)
-    
+
   } else {
-  
+
     if (!all(forest %in% FORESTNAMES)) {
       message("invalid forest: \n", toString(FORESTNAMES))
       forest <- select.list(FORESTNAMES, title="forest names", multiple=TRUE)
     }
   }
-    
-  
+
+
   ## Get forest filter
-  forest.filter <- paste0("OWNERCLASSIFICATION = 'USDA FOREST SERVICE' AND FORESTNAME IN(", 
+  forest.filter <- paste0("OWNERCLASSIFICATION = 'USDA FOREST SERVICE' AND FORESTNAME IN(",
                           toString(encodeString(forest, quote="'")), ")")
-  
+
   ## Get forest boundaries
   forest <- arcpullr::get_spatial_layer(url = URLalp,
                                         where = forest.filter)
-  
+
   ## check validation of polygons
   if (!any(sf::st_is_valid(forest))) {
     forest <- FIESTAutils::polyfix.sf(forest)
   }
-  
+
   return(forest)
 }
 
 
-
+#' get fires EDW
+#'
+#' @param fireyear Year
+#' @param minacres Minimum acres
+#' @export
 getfiresEDW <- function(fireyear, minacres = NULL) {
-  
+
   ## Final Fire Perimeter (All Years)
-  ## The FirePerimeterFinal polygon layer represents final mapped wildland fire perimeters. 
+  ## The FirePerimeterFinal polygon layer represents final mapped wildland fire perimeters.
   ## USDA Forest Service National Forest System Lands GIS and Fire personnel
-  
-  require(arcpullr)
-  require(FIESTA)
+
   #URLfire <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_FireOccurrenceAndPerimeter_01/MapServer/"
   URLfile <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_FireOccurrenceAndPerimeter_01/MapServer/10"
-  
+
   ## Define out_fields
   out_fields <- c("UNIQFIREID", "FIRENAME", "TOTALACRES", "FIREYEAR", "OWNERAGENCY")
-  
+
   if (fireyear == 2017) {
     year <- c(2017,2018)
   } else {
     year <- fireyear
   }
-  
+
   ## Build filter for fires
   fireyear.filter <- paste0("FIREYEAR IS NOT NULL AND ", FIESTAutils::getfilter("FIREYEAR", year, syntax="sql"))
-  
+
   if (!is.null(minacres)) {
     if (!is.numeric(minacres)) {
       stop("invalid minacres... must be numeric")
     }
-   fireyear.filter <- paste0(fireyear.filter, 
-                            " AND TOTALACRES >= ", minacres) 
+   fireyear.filter <- paste0(fireyear.filter,
+                            " AND TOTALACRES >= ", minacres)
   }
-  
-  
+
+
   ## Get fires
   fires <- arcpullr::get_spatial_layer(url = URLfile,
                                        out_fields = out_fields,
@@ -359,33 +376,33 @@ getfiresEDW <- function(fireyear, minacres = NULL) {
   if (fireyear == 2017) {
     fires[fires$FIREYEAR == 2017,]
   }
-  
-  
+
+
   # URLfire <- "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_FireOccurrenceAndPerimeter_01/MapServer/"
-  # 
+  #
   # fireyears <- 2016:2023
   # if (!fireyear %in% fireyears) {
-  #   stop("fire year must be between 2016 and 2023") 
+  #   stop("fire year must be between 2016 and 2023")
   # }
-  # end <- ifelse (fireyear == 2016, 5, 
-  #           ifelse (fireyear == 2017, 4, 
-  #              ifelse (fireyear == 2018, 3, 
-  #                  ifelse (fireyear == 2019, 17, 
-  #                      ifelse (fireyear == 2020, 20, 
-  #                          ifelse (fireyear == 2021, 24, 
+  # end <- ifelse (fireyear == 2016, 5,
+  #           ifelse (fireyear == 2017, 4,
+  #              ifelse (fireyear == 2018, 3,
+  #                  ifelse (fireyear == 2019, 17,
+  #                      ifelse (fireyear == 2020, 20,
+  #                          ifelse (fireyear == 2021, 24,
   #                               ifelse (fireyear == 2022, 27, 30)))))))
   # URLfireYEAR <- paste0(URLfire, end)
-  
-  
+
+
   ## Get fires
   #fire <- arcpullr::get_spatial_layer(url = URLfireYEAR,
   #                      out_fields = out_fields)
-  
+
   ## check validation of polygons
   if (!any(sf::st_is_valid(fires))) {
     fires <- FIESTAutils::polyfix.sf(fires)
   }
-  
+
   return(fires)
 }
 
